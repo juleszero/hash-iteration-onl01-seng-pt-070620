c09d8084c54b426160b2bc0ff121724108e93e75
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-16 00:41:28 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,8 +10,11 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids, age)
-  bir
+def age_appropriate_birthday(birthday_kids)
+    birthday_kids.each do |kids_name, age|
+      if age <= 12
+        puts 
+
 end
 
 
